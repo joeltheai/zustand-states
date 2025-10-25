e96b145a2bd481c3442da054515bbbc1e8a616cc
joeltheai <55612521+joeltheai@users.noreply.github.com>
2025-10-26 00:40:03 +0530

    Add navigation links to the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <nav className="flex gap-4">
+            <Link href="/">Home</Link>
+            <Link href="/testy">Testy</Link>
+          </nav>
+        </div>
         {children}
       </body>
     </html>
